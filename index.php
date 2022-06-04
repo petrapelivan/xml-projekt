15f9f670c056c915c6d41a90dab0665720ce93e8
petrapelivan <96172229+petrapelivan@users.noreply.github.com>
2022-06-04 13:29:58 +0200

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ function ispisi($username) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Studomat</title>
 </head>
 <body>
     <h1>Studomat</h1>
